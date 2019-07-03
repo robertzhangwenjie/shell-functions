221ce415b291d4b2bf7aea4ec602887c536ed03f
robert <robertzhangwenjie@gmail.com>
2019-07-04 01:17:02 +0800

    add function filter_file_with_suffix

---
+++ devops/filter_file_with_suffix.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# filter files by inputing a dir_path and the suffix of filename
+# print help doc
+if [[ $1 =~ ^-(h|H)$ ]];then
+    echo -e "usage: $0 [dir] [suffix ...] \nexample: $0 /tmp sh [war] [jar]"
+fi
+# get dir_name
+dir=$1
+if [ ! -d $dir ];then
+    echo "$dir is not a existing path, please input correct path"
+    exit 1
+fi
+# set default suffix_pattern 
+suffix_pattern_inital="^(war|jar)\$"
+suffix=$2
+# judge if $3 is not zero
+while [  -n "$3" ];do
+    suffix=${suffix}:$3
+    shift
+done
+# generate suffix_pattern by replace : with |
+if [  -n "$suffix" ];then
+    suffix_pattern=`echo "^(${suffix})\$" | sed 's@:@|@g' `
+fi
+# use defaullt pattern if no sufixx are passed in
+[ -z "$suffix" ] && suffix_pattern=$suffix_pattern_inital
+# use current dir as filter dir if no dir_name are passed in
+filenames=$( ls ${dir} )
+
+for filename in $filenames;do
+    if [[ ${filename##*.} =~ $suffix_pattern ]];then
+        echo $filename
+    fi
+done
