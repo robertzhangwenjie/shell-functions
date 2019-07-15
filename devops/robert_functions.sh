15e10af4bb96ddd67d0471d142cbdc454139b2c2
robert <robertzhangwenjie@gmail.com>
2019-07-15 11:35:07 +0800

    add new function

---
+++ devops/robert_functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this is a set of special functions robert used 
+. /etc/init.d/functions
+PS3="please select a menu number:"
+
+
+# del volumes of  sytem automatic created
+del_noself_created_volumes() {
+    docker volume ls | awk 'BEGIN{print "----------Volumes Listed Will Del----------"}/\w{50,100}/{print $2}END{print '\n'}'
+    volumes=$(docker volume ls | awk '/\w{50,100}/{print $2}')
+    echo -n "Are you ready to del all voluems listed, yes/no?"
+    read flag
+    [[ $flag =~ (no|No|N|n) ]] && exit 1
+    for volume in $volumes;do
+        docker volume rm $volume 
+        [ $? == 0 ] && action "rm volume $volume successful" /bin/true 
+        [ $? != 0 ] && action "rm volume $volume failed" /bin/false
+    done
+}
+
+menus='del_noself_created_volumes exit'
+select menu in $menus;do
+      echo -e "$menu\n"
+      eval $menu
+   done
+
+
