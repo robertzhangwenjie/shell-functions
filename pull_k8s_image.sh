f477b64a9b92482849e50e08c40a03fc7951c3e0
robert <robertzhangwenjie@gmail.com>
2019-07-20 12:40:31 +0800

    update

--- pull_k8s_image.sh
+++ pull_k8s_image.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 images=(
-k8s.gcr.io/kube-apiserver:v1.15.0
-k8s.gcr.io/kube-controller-manager:v1.15.0
-k8s.gcr.io/kube-scheduler:v1.15.0
-k8s.gcr.io/kube-proxy-amd64:v1.15.0
-k8s.gcr.io/pause:3.1
-k8s.gcr.io/etcd:3.3.10
-k8s.gcr.io/coredns:1.3.1
+kube-apiserver:v1.15.0
+kube-controller-manager:v1.15.0
+kube-scheduler:v1.15.0
+kube-proxy-amd64:v1.15.0
+pause:3.1
+etcd:3.3.10
+coredns:1.3.1
 )
 
 ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/cookcodeblog
