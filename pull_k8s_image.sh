e2a579c7a2d48e5d9425210e7d4102d8d597127e
robert <robertzhangwenjie@gmail.com>
2019-07-20 13:08:45 +0800

    update k8s dockerhub url

--- pull_k8s_image.sh
+++ pull_k8s_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# How to get k8s images whit dockerhub and github
+# https://blog.csdn.net/sjyu_ustc/article/details/79990858 
 images=(
 kube-apiserver:v1.15.0
 kube-controller-manager:v1.15.0
@@ -8,10 +10,11 @@ pause:3.1
 etcd:3.3.10
 coredns:1.3.1
 )
-
-ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/cookcodeblog
+DOCKERHUB_URL=mirrorgooglecontainers
+ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
+URL=${DOCKERHUB_URL}
 for imageName in ${images[@]} ; do
-    docker pull ${ALIYUN_URL}/$imageName
-    docker tag ${ALIYUN_URL}/$imageName k8s.gcr.io/$imageName
-    docker rmi ${ALIYUN_URL}/$imageName
+    docker pull ${URL}/$imageName
+    docker tag ${URL}/$imageName k8s.gcr.io/$imageName
+    docker rmi ${URL}/$imageName
 done
