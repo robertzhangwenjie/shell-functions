81238ea08edede5a43ab0ed52db14956ccc7ff4c
robert <robertzhangwenjie@gmail.com>
2019-07-20 12:37:55 +0800

    update k8s

--- pull_k8s_image.sh
+++ pull_k8s_image.sh
@@ -9,7 +9,9 @@ k8s.gcr.io/etcd:3.3.10
 k8s.gcr.io/coredns:1.3.1
 )
 
+ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/cookcodeblog
 for imageName in ${images[@]} ; do
-    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
-        docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
-    done
+    docker pull ${ALIYUN_URL}/$imageName
+    docker tag ${ALIYUN_URL}/$imageName k8s.gcr.io/$imageName
+    docker rmi ${ALIYUN_URL}/$imageName
+done
