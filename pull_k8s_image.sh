c4ba82ed9955a72de4422bc381effd79b2d47420
robertzhangwenjie <35749198+robertzhangwenjie@users.noreply.github.com>
2019-07-21 09:40:00 +0800

    Update pull_k8s_image.sh

--- pull_k8s_image.sh
+++ pull_k8s_image.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# How to get k8s images whit dockerhub and github
-# https://blog.csdn.net/sjyu_ustc/article/details/79990858 
 images=(
 kube-apiserver:v1.15.0
 kube-controller-manager:v1.15.0
 kube-scheduler:v1.15.0
-kube-proxy-amd64:v1.15.0
+kube-proxy:v1.15.0
 pause:3.1
 etcd:3.3.10
-coredns:1.3.1
 )
-DOCKERHUB_URL=mirrorgooglecontainers
-ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
-URL=${DOCKERHUB_URL}
+
+# ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
+ALIYUN_URL=mirrorgooglecontainers
 for imageName in ${images[@]} ; do
-    docker pull ${URL}/$imageName
-    docker tag ${URL}/$imageName k8s.gcr.io/$imageName
-    docker rmi ${URL}/$imageName
+    docker pull ${ALIYUN_URL}/$imageName
+    docker tag ${ALIYUN_URL}/$imageName k8s.gcr.io/$imageName
+    docker rmi ${ALIYUN_URL}/$imageName
 done
+docker pull coredns/coredns:1.3.1
+docker tag coredns/coredns:1.3.1 k8s.gcr.io/coredns:1.3.1
+docker rmi coredns/coredns:1.3.1
