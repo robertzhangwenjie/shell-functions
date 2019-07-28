ff6190e55a8c99229344bdfa34d7b4d06a102389
root <root@localhost.localdomain>
2019-07-29 03:34:19 +0800

    update install k8s scripts

--- rsync_inotify.sh
+++ rsync_inotify.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 function sync() {
-	a="inotifywait -mrq -e create,delete,modify /filesrc"
-	b="rsync -avz /filesrc/* root@192.168.43.240:/filedest"
+	a="inotifywait -mrq -e create,delete,modify /data/gitlab-master"
+	b="rsync -avz /data/gitlab-master/* root@192.168.43.240:/gitlab-master/"
 	#监控该目录的所有文件是否有变动
 	$a | while read directory event file; do
   		$b
