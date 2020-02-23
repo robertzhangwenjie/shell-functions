b342c5456bc01d97b6fe0385bdb57bb3f343efff
robertzhang <robertzhangwenjie@gmail.com>
2020-02-23 10:34:53 +0800

    update prompt of pull_k8s_image.sh

--- k8s_install/pull_k8s_image.sh
+++ k8s_install/pull_k8s_image.sh
@@ -9,7 +9,7 @@ function azk8spull()
 {
 	image=$1
 	if [ -z $image ]; then
-		echo "## azk8spull image name cannot be null."
+		echo "***Image name cannot be null."
 	else
 		array=(`echo $image | tr '/' ' '` )
 
@@ -27,7 +27,7 @@ function azk8spull()
 			elif [ "${array[0]}"x = "quay.io"x ]; then
 				domainName=${QUAY_PROXY}
 			else
-				echo '## azk8spull can not support pulling $image right now.'
+				echo '***Can not support pull $image right now.'
 			fi
 		elif [ ${#array[*]} -eq 2 ]; then
 			if [ "${array[0]}"x = "k8s.gcr.io"x ]; then
@@ -44,18 +44,19 @@ function azk8spull()
 				repoName="library"
 				imageName=${array[0]}
 		else
-			echo '## azk8spull can not support pulling $image right now.'
+			echo '***Warning: Can not support pull $image right now.'
 		fi
 		if [ $domainName != "" ]; then
-			echo "## azk8spull try to pull image from mirror $domainName/$repoName/$imageName."
+			echo "***Pulling image from mirror $domainName/$repoName/$imageName."
 			docker pull  $domainName/$repoName/$imageName
 			if [ $? -eq 0 ]; then
-				echo "## azk8spull try to tag $domainName/$repoName/$imageName to $image."
+				echo "***Taging $domainName/$repoName/$imageName to $image."
 				docker tag $domainName/$repoName/$imageName $image
 				if [ $? -eq 0 ]; then
-					echo '## azk8spull finish pulling. '
+					echo "***Pull $image successfully."
 				fi
 			fi
 		fi
 	fi
 }
+azk8spull $@
