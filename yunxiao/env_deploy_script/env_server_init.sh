e9bbfc981dad655bea8d8d6285e51b055779e63a
robertzhangwenjie@gmail.com <robertzhangwenjie@gmail.com>
2020-08-27 15:26:09 +0800

    extract common functions for env script

--- yunxiao/env_deploy_script/env_server_init.sh
+++ yunxiao/env_deploy_script/env_server_init.sh
@@ -2,7 +2,7 @@
 ###
 # @Author: robert zhang
 # @Date: 2020-08-25 11:34:37
- # @LastEditTime: 2020-08-27 10:36:55
+ # @LastEditTime: 2020-08-27 15:25:37
  # @LastEditors: robert zhang
 # @Description: 初始化账号配置,需要root权限
 # @
@@ -12,7 +12,7 @@ YUNXIAO_GOURP=yunxiao
 UPLOAD_URL="http://package.switch.aliyun.com:9090/upload"
 DOWNLOAD_URL="http://package.switch.aliyun.com:8088"
 ENV_SCRIPT="env_script.zip"
-UPLOAD_SCRIPT="curl -X POST -F warName=@${ENV_SCRIPT} -F crid=0  -F appName=yunxiao -F buildNum=1 -F compileId=1 ${UPLOAD_SCRIPT}"
+UPLOAD_SCRIPT="curl -X POST -F warName=@${ENV_SCRIPT} -F crid=0  -F appName=yunxiao -F buildNum=1 -F compileId=1 ${UPLOAD_URL}"
 
 # 获取当前文件的绝对路径
 WORK_PATH=$(
@@ -24,40 +24,42 @@ source $WORK_PATH/functions.sh
 
 # 上传env_script
 upload_env_script() {
-  echo "压缩:${ENV_SCRIPT}"
-  zip -qr ${ENV_SCRIPT} env_script/*
-
-  echo "上传:$UPLOAD_SCRIPT"
-  ENV_SCRIPT_ADDRESS=$(eval $UPLOAD_SCRIPT)
+  log_info "压缩 env_script"
+  do_it zip -qr ${ENV_SCRIPT} env_script/*
+  log_info "上传 ${ENV_SCRIPT}"
+  ENV_SCRIPT_ADDRESS=`eval $UPLOAD_SCRIPT`
 }
 
 # 下载env_scropt脚本包
 get_env_script() {
+  log_info "downloading ${DOWNLOAD_URL}/${ENV_SCRIPT_ADDRESS}"
   do_it wget -nv -P ${HOME} ${DOWNLOAD_URL}/${ENV_SCRIPT_ADDRESS}
+  log_info "downloaded successful"
 }
 
 # 添加sudo权限
 add_sudo_cfg() {
-  local sudo_cfg="@*"
-  grep "^$sudo_cfg" /etc/sudoers >/dev/null
+  local sudo_cfg="$*"
+  grep "^$sudo_cfg" /etc/sudoers >/dev/null 2>&1
   local isCfgExist=$?
 
   # 判断是否拥有sudo权限，如果没有就添加
-  if [ isCfgExist -eq 0 ]; then
-    action "Add $sudo_cfg" /bin/true
+  if [ $isCfgExist -eq 0 ]; then
+    action "Add sudo cfg: $sudo_cfg" /bin/true
   else
     echo "$sudo_cfg" >>/etc/sudoers &&
-      action "Add $sudo_cfg" /bin/true ||
-      action "Add $sudo_cfg" /bin/false
+      action "Add sudo cfg: $sudo_cfg" /bin/true ||
+      action "Add sudo cfg: $sudo_cfg" /bin/false
   fi
 }
 
 # 添加sudo组，并设置sudo权限
 add_sudo_group() {
 
+  log_info "添加用户组"
   groupadd ${YUNXIAO_GOURP} >/dev/null 2>&1
 
-  # 设置yunxiao用户组拥有sudo权限，在执行docker命令时不需要密码
+  log_info "设置sudo权限"
   local add_group_cfg="%${YUNXIAO_GROUP:-yunxiao} ALL=(ALL) NOPASSWD:/usr/bin/docker,/bin/cp"
   local add_env_cfg='Defaults env_keep += "PATH"'
 
@@ -70,16 +72,17 @@ add_sudo_group() {
 install_env_script() {
 
   local username=$1
-  # 复制文件
-  do_it /bin/cp -rf /root/$ENV_SCRIPT /home/$username/
+  log_info "复制${ENV_SCRIPT} 到 /home/${username}"
+  do_it /bin/cp -rf /root/$ENV_SCRIPT /home/$username
   do_it unzip -o -d /home/$username /home/$username/$ENV_SCRIPT
   # 授权
-  do_it chown $username:$username -R /home/$username/
+  do_it chown $username:$username -R /home/$username
 }
 
 # 添加用户
 add_user() {
   local username=$1
+  log_info "添加用户${username}"
   do_it useradd $username -p $username -G ${YUNXIAO_GOURP}
   install_env_script $username
 }
@@ -92,6 +95,6 @@ get_env_script
 # 添加sudo执行权限组
 add_sudo_group
 # 添加用户，并copy脚本到对应家目录下
-if [ -n $1 ]; then
+if [ -n "$1" ]; then
   add_user $1
 fi
--- yunxiao/env_deploy_script/functions.sh
+++ yunxiao/env_deploy_script/functions.sh
@@ -2,22 +2,33 @@
 ###
  # @Author: robert zhang
  # @Date: 2020-08-27 09:36:23
- # @LastEditTime: 2020-08-27 09:57:10
+ # @LastEditTime: 2020-08-27 15:17:24
  # @LastEditors: robert zhang
  # @Description: 部署脚本公共函数
  # @
 ### 
 source /etc/rc.d/init.d/functions
 
+log_info() {
+  echo "[INFO] $1"
+}
+
+log_warning() {
+  echo -e "\033[33m[WARNING]\033[0m" $1
+}
+
+log_error() {
+  echo -e "\033[31m[ERROR]\033[0m" $1
+}
+
 # 执行语句并打印失败或成功
 do_it() {
   local execute_cmd=$@
-  eval $execute_cmd
-
+  eval $@
   local isSuccess=$?
   if [ $isSuccess -eq 0 ];then
     action "$execute_cmd"  /bin/true
-    return 0
+    return 
   else
     action "$execute_cmd"  /bin/false
     return 1
